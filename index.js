4e947dba19d611c0733896eeb8c76c53f405ffa7
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-05-24 12:09:15 +0100

    Read user input from the console

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+console.log('Please enter some input:');
+const response = readline.prompt();
+console.log('You entered: ' + response);
